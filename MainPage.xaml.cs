791b712d206740ffd41a902b487a3169e77f5a6c
Joey <j.ampong.482578@umindanao.edu.ph>
2025-11-16 05:42:23 +0800

    Initial commit - MedFlow UI and pages

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace IT_13FinalProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
