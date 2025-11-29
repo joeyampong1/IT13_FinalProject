d96b4c0e8f1473eebaae1d27a16f64ed56c26f6f
Joey <j.ampong.482578@umindanao.edu.ph>
2025-11-29 23:42:15 +0800

    Merged latest changes with local work

--- obj/Debug/net9.0-windows10.0.19041.0/win10-x64/IT_13FinalProject.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows10.0.19041.0/win10-x64/IT_13FinalProject.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IT_13FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+650695311cb9b9741b29d00d27b94d3cbb640093")]
 [assembly: System.Reflection.AssemblyProductAttribute("IT_13FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IT_13FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
